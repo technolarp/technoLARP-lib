7150d389ac5c598ad745bc61a475943b64bca8ab
marcelinDelcour <marcelin.delcour@gmail.com>
2021-12-07 23:36:58 +0100

    MDE: remove TS bombe_7segment_01 1

--- technolarp_7segment.cpp
+++ technolarp_7segment.cpp
@@ -5,12 +5,28 @@ TM1637Display display_7seg(CLK_PIN, DIO_PIN);
 
 M_7segment::M_7segment()
 {
-  display_7seg.setBrightness(0x0b);
-  display_7seg.clear();
-  
-  blinkDoublePoint = false;
-  blinkAffichage = true;
-  blinkMinutesOuSecondes = true;
+	display_7seg.setBrightness(0x0b);
+	display_7seg.clear();
+	
+	
+	blinkAffichage = true;
+	blinkMinutesOuSecondes = true;
+	
+	animationActive=false;
+	displayStatus=true;
+	iterations = 0;
+	
+	interval = 100;
+	previousMillis = 0;
+	
+	intervalDoublePoint = 500;
+	previousMillisDoublePoint = 0;
+	statutDoublePoint = false;
+	blinkDoublePoint = false;
+		
+	animForever = false;
+	
+	animActuelle = ANIM_NONE;
 }
 
 
@@ -75,28 +91,12 @@ void M_7segment::showTempsRestant(int16_t tempsRestant)
     }
   }
   
-  if (blinkDoublePoint)
+  if (statutDoublePoint)
   {
 	data[1] = display_7seg.encodeDigit(minutes%10);
   }
   
   display_7seg.setSegments(data);
-}
-
-
-void M_7segment::showExplosion()
-{
-  uint8_t SEG_VIDE[] = {0x00, 0x00, 0x00, 0x00};
-  uint8_t SEG_BOOM[] = {0x7C, 0x5C, 0x5C, 0x54}; // b, o, o, n
-	
-  if (blinkAffichage)
-  {
-	display_7seg.setSegments(SEG_BOOM, 4, 0);
-  }
-  else
-  {
-	display_7seg.setSegments(SEG_VIDE, 4, 0);
-  }
 }
 
 void M_7segment::showExplosee()
@@ -113,4 +113,147 @@ void M_7segment::showSafe()
   // afficher "S A F E" sur le 4*7segment
   uint8_t SEG_SAFE[] = {0x6D, 0x77, 0x71, 0x79};
   display_7seg.setSegments(SEG_SAFE);
+}
+
+
+
+void M_7segment::animationBoomStart(uint16_t intervalToSet, uint16_t dureeToSet)
+{
+	animActuelle = ANIM_BOOM;
+	animationActive=true;
+	displayStatus=true;
+	iterations = dureeToSet / intervalToSet;
+	
+	interval = intervalToSet;
+	animForever = false;
+	
+	previousMillis = millis();
+}
+
+void M_7segment::animationBoom()
+{
+	uint8_t SEG_VIDE[] = {0x00, 0x00, 0x00, 0x00};
+	uint8_t SEG_BOOM[] = {0x7C, 0x5C, 0x5C, 0x54}; // b, o, o, n
+	
+	if (displayStatus)
+	{
+		display_7seg.setSegments(SEG_BOOM, 4, 0);
+	}
+	else
+	{
+		display_7seg.setSegments(SEG_VIDE, 4, 0);
+	}
+}
+
+void M_7segment::animationBoomEnd()
+{
+	
+}
+
+void M_7segment::animationBlinkStart(uint16_t intervalToSet, uint16_t dureeToSet, bool forever)
+{
+	animActuelle = ANIM_BLINK;
+	animationActive=true;
+	displayStatus=true;
+	iterations = dureeToSet / intervalToSet;
+	
+	interval = intervalToSet;
+	animForever = forever;
+	
+	previousMillis = millis();
+}
+
+void M_7segment::animationBlink()
+{
+	
+}
+
+void M_7segment::animationBlinkEnd()
+{
+	animForever = false;
+}
+
+bool M_7segment::isAnimActive()
+{
+	if (animActuelle == ANIM_NONE)
+	{
+		return(false);
+	}
+	else
+	{
+		return(true);
+	}
+}
+
+void M_7segment::setAnimation(uint8_t toSet)
+{
+	animActuelle = toSet;
+}
+
+void M_7segment::switchAnim(uint8_t anim)
+{
+	switch (anim) 
+    {
+      case ANIM_BOOM:
+        animationBoom();
+      break;
+	  
+      default:
+        // nothing to do
+      break;
+    }
+}
+
+void M_7segment::switchAnimEnd(uint8_t anim)
+{
+	switch (anim) 
+    {
+      case ANIM_BOOM:
+        animationBoomEnd();
+      break;
+	  
+      default:
+        // nothing to do
+      break;
+    }
+}
+
+void M_7segment::updateAnimation()
+{
+	if (blinkDoublePoint)
+	{
+		if(millis() - previousMillisDoublePoint > intervalDoublePoint)
+		{
+			previousMillisDoublePoint = millis();
+			statutDoublePoint = !statutDoublePoint;
+		}
+	}
+	
+	if (animationActive)
+	{
+		if(millis() - previousMillis > interval)
+		{
+			previousMillis = millis();
+			displayStatus = !displayStatus;
+			
+			switchAnim(animActuelle);
+			
+			if (iterations>0)
+			{
+				iterations-=1;
+			}
+			
+			if ( (iterations==0) && animForever )
+			{
+				iterations = 1;
+			}
+			
+			if (iterations==0)
+			{
+				animationActive = false;
+				switchAnimEnd(animActuelle);
+				animActuelle = ANIM_NONE;
+			}
+		}
+	}
 }
--- technolarp_7segment.h
+++ technolarp_7segment.h
@@ -7,22 +7,54 @@
 class M_7segment
 {
   private:
-	bool blinkDoublePoint;
 	bool blinkAffichage;
 	bool blinkMinutesOuSecondes;
+	
+	bool animationActive;
+	bool displayStatus;
+	uint16_t iterations;
+	
+	uint16_t interval;
+	uint32_t previousMillis;
+	
+	uint32_t previousMillisDoublePoint;
+	uint32_t intervalDoublePoint;
+	bool statutDoublePoint;
+	bool blinkDoublePoint;
+	
+	bool animForever;
+	
+	enum {ANIM_NONE, ANIM_BOOM, ANIM_BLINK};
+	uint8_t animActuelle;
 	
   public:  
 	M_7segment();
 	
 	void showDecimal(uint16_t aDecimal, bool leadingZero);
+	
 	void setDoublePoint(bool toSet);
 	bool getDoublePoint();
 	
 	void showTempsRestant(int16_t tempsRestant);
 	
-	void showExplosion();
+	void updateAnimation();
+	void switchAnim(uint8_t anim);
+	void switchAnimEnd(uint8_t anim);
+	
+	bool isAnimActive();
+	void setAnimation(uint8_t toSet);
+	
+	void animationBoomStart(uint16_t intervalToSet, uint16_t dureeToSet);
+	void animationBoom();
+	void animationBoomEnd();
+	
+	void animationBlinkStart(uint16_t intervalToSet, uint16_t dureeToSet, bool forever);
+	void animationBlink();
+	void animationBlinkEnd();
+	
 	void showExplosee();
 	void showSafe();
+	
 	
 	void setBlinkAffichage(bool toSet);
 	bool getBlinkAffichage();
